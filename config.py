eca80331bdbba04f92bc7652ff465f835623d0e2
Ksenia Dubrovina <kseniady97@gmail.com>
2025-10-05 15:25:48 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+# Токен Telegram-бота
+BOT_TOKEN = "ВАШ BOT_TOKEN"
+
+# API ключ ElevenLabs
+ELEVEN_API_KEY = "ВАШ ELEVEN_API_KEY"
+
+# Базовый URL API
+ELEVEN_API_URL = "https://api.elevenlabs.io/v1"
